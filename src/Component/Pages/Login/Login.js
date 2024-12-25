371e1dd0b3b13418331797190b47ad369e55514a
Ali Ashooriyoon <72642763+AliAshooriyoon@users.noreply.github.com>
2024-12-25 21:35:10 +0330

    Some changes on Login and routes

---
+++ src/Component/Pages/Login/Login.js
@@ -0,0 +1,14 @@
+// import "Login.css"
+import "./Login.css";
+const Login = () => {
+  return (
+    <div className="login">
+      Login Page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+      architecto assumenda, atque distinctio dolorem doloremque explicabo iste
+      itaque provident ratione sint sit unde. Ex ipsa quaerat quod repudiandae
+      rerum totam.
+    </div>
+  );
+};
+
+export default Login;
--- src/Component/Pages/routes.js
+++ src/Component/Pages/routes.js
@@ -1,9 +1,11 @@
+import Login from "./Login/Login";
 import Home from "./Home/Home";
 import Header from "./Header/Header";
 import Download from "./Download/Download";
 const Routes = [
   { element: <Home />, path: "/" },
   { element: <Download />, path: "/download" },
+  { element: <Login />, path: "/account/login" },
 ];
 
 export default Routes;
